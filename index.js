432901d3434815039421ddd0726115645e8c97be
boyninja1555 <149730905+boyninja1555@users.noreply.github.com>
2024-06-01 09:42:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,14 +28,8 @@ const server = http.createServer(app)
 const io = socketIo(server)
 
 io.on("connection", (socket) => {
-    console.log("A user has entered the chat")
-
     socket.on("chat message", ({ username, msg }) => {
         io.emit("chat message", `<${username}> ${msg}`)
-    })
-
-    socket.on("disconnect", () => {
-        console.log("A user has left the chat")
     })
 })
 
